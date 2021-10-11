f6768be34ff4d67b8177bbe82bb4f9a3da6eab16
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-11 23:15:14 +0200

    principal component for pokemons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonsComponent } from './pokemons/pokemons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pokemons', component: PokemonsComponent },
+  { path: '**', redirectTo: '/pokemons', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const ArrayOfComponents = [PokemonsComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, ArrayOfComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PokemonsComponent } from './pokemons/pokemons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonsComponent,
+    ArrayOfComponents
   ],
   imports: [
     BrowserModule,
