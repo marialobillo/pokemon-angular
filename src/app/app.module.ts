52c60602517df5243e6dd053412479b4e199bfe3
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-11 22:55:50 +0200

    cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from 'src/shared/material.module';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListingPokemonsComponent,
-    ViewPokemonComponent,
-    PokemonsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
