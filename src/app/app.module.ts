60fb77a27c9c25e453da0c9397171f3771659e4f
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-12 17:38:54 +0200

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListComponent } from './components/list/list.component';
+import { ViewComponent } from './components/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
