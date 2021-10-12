fc3cc4bd978c212366cb2cb30145caca946c819b
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-12 18:17:48 +0200

    setting up the service

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  baseUrl: string = environment.base_url;
+
+  constructor(private http: HttpClient) { }
+
+  getPokemons(query: any){
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${query}`);
+  }
 }
