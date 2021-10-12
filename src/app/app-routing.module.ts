456e795877cfdaff579c8f7980ef1f88355c5c89
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-12 18:03:15 +0200

    adding base_url variable to environments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { ViewComponent } from './components/view/view.component';
 
 const routes: Routes = [
- 
+ { path: 'pokemons', component: ListComponent },
+ { path: 'pokemons/:id', component: ViewComponent },
+ { path: '', pathMatch: 'full', redirectTo: 'pokemons' },
+ { path: '**', pathMatch: 'full', redirectTo: 'pokemons' }
 ];
 
 @NgModule({
