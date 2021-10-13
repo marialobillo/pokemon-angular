3767dac24eae9aac129f7f3c8d38c20c85e03f8a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-13 17:10:48 +0200

    adding a pokemon service for get the pokemon list

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { PokemonList } from '../models/pokemon.detail';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,12 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  // getPokemonsList(offset:number, limit: number = 20): Observable<PokemonList[]>
+  getPokemonsList(offset:number, limit: number = 20): Observable<PokemonList[]>{
+    return this.http.get<PokemonList[]>(`${this.apiUrl}pokemon?offset=${offset}&limit=${limit}`)
+      .pipe(
+        map((x: any) => x.results)
+      );
+  }
 
   
 }
