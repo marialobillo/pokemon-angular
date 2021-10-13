7b30023e44203c070c6354b6544add523d9cbc28
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-13 19:55:22 +0200

    fixing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ListComponent } from './components/list/list.component';
-import { ViewComponent } from './components/view/view.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from 'src/shared/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    ListComponent,
-    ViewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
