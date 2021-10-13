aa51a90957d887313d835e54bc0b5a20bdfc1a1c
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-13 17:16:06 +0200

    get pokemon detail and fixing the slash

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { PokemonList } from '../models/pokemon.detail';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { PokemonDetail } from '../models/pokemon.list';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +16,14 @@ export class PokemonService {
   constructor(private http: HttpClient) { }
 
   getPokemonsList(offset:number, limit: number = 20): Observable<PokemonList[]>{
-    return this.http.get<PokemonList[]>(`${this.apiUrl}pokemon?offset=${offset}&limit=${limit}`)
+    return this.http.get<PokemonList[]>(`${this.apiUrl}/pokemon?offset=${offset}&limit=${limit}`)
       .pipe(
         map((x: any) => x.results)
       );
+  }
+
+  getPokemonDetail(pokemon: number | string): Observable<PokemonDetail> {
+    return this.http.get<PokemonDetail>(`${this.apiUrl}/pokemon/${pokemon}`);
   }
 
   
