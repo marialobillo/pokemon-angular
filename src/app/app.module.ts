e9926fa8d1cfaa7d8a2dfe8ee08c66927208961b
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-13 19:56:54 +0200

    components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PokemonDetailComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
